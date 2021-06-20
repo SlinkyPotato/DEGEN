47a535320bf786a482131f3bafe04bdcdcd16aaa
Joshua Alexander <joshua@nonsensecodes.io>
2021-06-20 10:11:41 -0700

    Feature/config and clean up (#27)
    * removed unused dependencies
    
    * prettier config
    
    * eslint config
    
    * run prettier
    
    * run linter with fix option
    
    * cleanup empty space and enable db connection
    
    * comment out comment
    
    * db connection
    
    * comment out db. to be continued...
    
    * comment out db variable declaration
    
    * added node to eslint config env
    
    * override test files
    
    * exclude test files
    
    * ignore test files
    
    * eslint config
    
    * ignore test files
    
    * lint run with fix
    
    * no var requires
    
    * setting environments
    
    * setting env
    
    * run linter
    
    * comment out unused vars
    
    * unused var
    
    * code cleanup
    
    * remove old config file
    
    Co-authored-by: Joshua Alexander <joshua@nonsensecodes.co>

--- src/app/db.js
+++ src/app/db.js
@@ -1,40 +1,46 @@
 const MongoClient = require('mongodb').MongoClient;
 
 const state = {
-    db: null,
-  }
+	db: null,
+};
 
-let db = {
-  
-  connect(url, done) {
-    if (state.db) {
-      return done();
-    } else {
-      MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-        if (err) {
-          return console.error(err);
-        } else {
-          // console.log(client);
-          state.db = client.db('banklessDAO');
-          done();
-        }
-      });
-    }
-  },
+const db = {
+	connect(url, done) {
+		if (state.db) {
+			return done();
+		}
+		else {
+			MongoClient.connect(
+				url,
+				{ useUnifiedTopology: true },
+				(err, client) => {
+					if (err) {
+						return console.error(err);
+					}
+					else {
+						// console.log(client);
+						state.db = client.db('bankless');
+						done();
+					}
+				},
+			);
+		}
+	},
 
-  get() {
-    return state.db;
-  },
+	get() {
+		return state.db;
+	},
 
-  close(done) {
-    if (state.db) {
-      state.db.close((err, result) => {
-        state.db = null;
-        state.mode = null;
-        done(err);
-      });
-    }
-  }
-}
+	close(done) {
+		if (state.db) {
+			state.db.close((err) => {
+			// state.db.close((err, result) => {
+				state.db = null;
+				state.mode = null;
+				done(err);
+			});
+		}
+	},
+};
 
 module.exports = db;

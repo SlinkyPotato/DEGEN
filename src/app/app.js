685f7c324c152ee3303eb1626d3969458164e8b2
Austin Mitchem <austinmitchem@outlook.com>
2021-06-28 22:04:28 -0500

    update guest pass database on role change

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,7 @@
 // Libs
 const { MessageEmbed } = require('discord.js');
 const { CommandoClient } = require('discord.js-commando');
+const constants = require('./constants');
 
 // Background services
 const GuestPassService = require('./service/GuestPassService.js');
@@ -45,6 +46,23 @@ client.on('guildMemberAdd', (member) => {
 	member.guild.channels
 		.find((c) => c.name === 'welcome')
 		.send(`Welcome to the DAO, <@${member.user.id}>`);
+});
+
+client.on('guildMemberUpdate', (oldMember, newMember) => {
+	// Check if member roles were updated
+	if (oldMember.roles !== newMember.roles) {
+		// Check if guest pass was added or removed
+		const newMemberHasGuestPass = newMember.roles.cache.some(role => role.name === constants.DISCORD_ROLE_GUEST_PASS);
+		const oldMemberHasGuestPass = oldMember.roles.cache.some(role => role.name === constants.DISCORD_ROLE_GUEST_PASS);
+		if (newMemberHasGuestPass && !oldMemberHasGuestPass) {
+			// Guest pass was added
+			GuestPassService.updateNotionGuestPassDatabase(newMember.user.tag, true);
+		}
+		else if (oldMemberHasGuestPass && !newMemberHasGuestPass) {
+			// Guest pass was removed
+			GuestPassService.updateNotionGuestPassDatabase(newMember.user.tag, false);
+		}
+	}
 });
 
 // filter raw packet data for reactions

de145efc1e94081ec03283f48cecd9ac129b800e
Austin Mitchem <austinmitchem@outlook.com>
2021-07-10 23:22:34 -0500

    run formating

--- src/app/app.js
+++ src/app/app.js
@@ -4,35 +4,35 @@ const Discord = require('discord.js');
 const path = require('path');
 const fs = require('fs');
 
-const client = new Discord.Client()
+const client = new Discord.Client();
 
 const creator = new SlashCreator({
 	applicationID: process.env.DISCORD_BOT_APPLICATION_ID,
 	publicKey: process.env.DISCORD_BOT_PUBLIC_KEY,
-	token: process.env.DISCORD_BOT_TOKEN
+	token: process.env.DISCORD_BOT_TOKEN,
 });
   
 // Register command handlers
 creator
 	.withServer(
 		new GatewayServer(
-			(handler) => client.ws.on('INTERACTION_CREATE', handler)
-		)
+			(handler) => client.ws.on('INTERACTION_CREATE', handler),
+		),
 	)
 	.registerCommands(fs.readdirSync(path.join(__dirname, 'commands'))
 		.map(file => {
-			return new (require(`./commands/${file}`))(creator, client)
-		})
+			return new (require(`./commands/${file}`))(creator, client);
+		}),
 	)
-	.syncCommands()
+	.syncCommands();
 
 // Register event handlers
 fs.readdir(path.join(__dirname, 'events'), (err, files) => {
 	if (err) return console.error(err);
 	files.forEach(file => {
-		if (!file.endsWith(".js")) return;
+		if (!file.endsWith('.js')) return;
 		const event = require(`./events/${file}`);
-		let eventName = file.split(".")[0];
+		const eventName = file.split('.')[0];
 		if (event.once) {
 			client.once(eventName, (...args) => event.execute(...args, client));
 		} else {

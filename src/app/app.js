2fd4ce85cfc663741fc0c09c283cb910c3eac0fb
Joshua Alexander <joshua@nonsensecodes.io>
2021-07-11 09:32:54 -0700

    Tighten up the code a bit in app.js

--- src/app/app.js
+++ src/app/app.js
@@ -14,11 +14,9 @@ const creator = new SlashCreator({
   
 // Register command handlers
 creator
-	.withServer(
-		new GatewayServer(
-			(handler) => client.ws.on('INTERACTION_CREATE', handler),
-		),
-	)
+	.withServer(new GatewayServer((handler) => {
+		client.ws.on('INTERACTION_CREATE', handler),
+	}))
 	.registerCommands(fs.readdirSync(path.join(__dirname, 'commands'))
 		.map(file => {
 			return new (require(`./commands/${file}`))(creator, client);

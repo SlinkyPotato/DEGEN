54a4c5c06edc5c5954de57c90efff4552c3720a1
Brian Patino <patinobrian@gmail.com>
2021-07-16 23:39:26 -0400

    add bounty command regex validation

--- src/app/commands/bounty/Bounty.ts
+++ src/app/commands/bounty/Bounty.ts
@@ -1,6 +1,10 @@
-import { CommandContext, SlashCreator } from 'slash-create';
+import { CommandContext, CommandOptionType, SlashCommand, SlashCreator } from 'slash-create';
+import db from '../../db/db';
+import { MongoError } from 'mongodb';
 
-const { SlashCommand, CommandOptionType } = require('slash-create');
+const CREATE_SUMMARY_REGEX = /^[\w\s.!@#$%&,?']{1,250}$/;
+const ALLOWED_CURRENCIES = ['ETH', 'BANK'];
+const MAXIMUM_REWARD = 100000000;
 
 module.exports = class Bounty extends SlashCommand {
 	constructor(creator: SlashCreator) {
@@ -13,14 +17,15 @@ module.exports = class Bounty extends SlashCommand {
 					type: CommandOptionType.STRING,
 					name: 'operation',
 					description: '(list|create|claim)',
+					required: true,
 				},
 				{
-					type: CommandOptionType.STRING,
+					type: CommandOptionType.SUB_COMMAND,
 					name: 'create-summary',
 					description: 'What would you like to be worked on?',
 				},
 				{
-					type: CommandOptionType.STRING,
+					type: CommandOptionType.SUB_COMMAND,
 					name: 'create-reward',
 					description: 'What is the reward? (i.e 100 BANK)',
 				},
@@ -39,11 +44,47 @@ module.exports = class Bounty extends SlashCommand {
 		case 'list':
 			return ctx.send('there are zero bounties...');
 		case 'create':
-			return ctx.send('bounty created with id: ');
+			return module.exports.handleCreateBounty(ctx);
 		case 'claim':
 			return ctx.send('bounty claimed for id: ');
 		default:
 			return ctx.send('no bounty for you! go away');
 		}
 	}
+};
+
+module.exports.handleCreateBounty = async (ctx: CommandContext) => {
+	const summary = ctx.options['create-summary'];
+
+	if (summary == null || !CREATE_SUMMARY_REGEX.test(summary)) {
+		return ctx.send('' +
+			'Please enter a valid create-summary value: \n ' +
+			'- 250 characters maximum\n ' +
+			'- alphanumeric\n ' +
+			'- special characters: .!@#$%&,?');
+
+	}
+
+	const createReward = ctx.options['create-reward'];
+	const [reward, rewardSymbol] = (createReward != null) ? createReward.split(' ') : [null, null];
+	const rewardNumber = Number(reward);
+
+	if (rewardNumber != Number.NaN || rewardNumber <= 0 || rewardNumber > MAXIMUM_REWARD
+		|| !ALLOWED_CURRENCIES.includes(rewardSymbol)) {
+		return ctx.send('' +
+			'Please enter a valid create-reward value: \n ' +
+			'- 100 million maximum currency\n ' +
+			'- accepted currencies: ETH, BANK');
+	}
+
+	// db.connect(process.env.MONGODB_URI, async (error: MongoError) => {
+	// 	if (error) {
+	// 		console.log('ERROR', error);
+	// 		return;
+	// 	}
+	//
+	//
+	//
+	// });
+	return ctx.send('.');
 };

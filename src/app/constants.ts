3f7c45d5596f33a652ca4fcdfd761f97b1b8ec08
Austin Mitchem <austinmitchem@outlook.com>
2021-07-20 00:18:58 -0500

    refactor guildMemberUpdate

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,5 @@
 export default Object.freeze({
 	DISCORD_ROLE_GUEST_PASS: 'Guest Pass',
+	DISCORD_ROLE_DEVELOPERS_GUILD: 'Developer\'s Guild',
 	DB_COLLECTION_GUEST_USERS: 'guestUsers',
 });
--- src/app/events/guildMemberUpdate.ts
+++ src/app/events/guildMemberUpdate.ts
@@ -2,27 +2,72 @@
  * Handler for Discord event `guildMemberUpdate`.
  */
 
+import { Collection, GuildMember, Role, Snowflake } from 'discord.js';
+import { Client } from '@notionhq/client';
 import { updateNotionGuestPassDatabase } from '../service/GuestPassService';
 import constants from '../constants';
+
+const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
 module.exports = {
 	name: 'guildMemberUpdate',
 	once: false,
 
-	async execute(oldMember, newMember) {
-		// Check if member roles were updated
-		if (oldMember.roles !== newMember.roles) {
-			// Check if guest pass was added or removed
-			const newMemberHasGuestPass = newMember.roles.cache.some(role => role.name === constants.DISCORD_ROLE_GUEST_PASS);
-			const oldMemberHasGuestPass = oldMember.roles.cache.some(role => role.name === constants.DISCORD_ROLE_GUEST_PASS);
-			if (newMemberHasGuestPass && !oldMemberHasGuestPass) {
-				// Guest pass was added
-				updateNotionGuestPassDatabase(newMember.user.tag, true);
-			}
-			else if (oldMemberHasGuestPass && !newMemberHasGuestPass) {
-				// Guest pass was removed
-				updateNotionGuestPassDatabase(newMember.user.tag, false);
-			}
+	async execute(oldMember: GuildMember, newMember: GuildMember) {
+		console.log('Guild member updated')
+		const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+		if (removedRoles.size > 0) { 
+			console.log(`The roles ${removedRoles.map(r => r.name)} were removed from ${oldMember.displayName}.`);
+			module.exports.handleRolesRemoved(newMember, removedRoles);
+		}
+
+		const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+		if (addedRoles.size > 0) {
+			console.log(`The roles ${addedRoles.map(r => r.name)} were added to ${oldMember.displayName}.`);
+			module.exports.handleRolesAdded(newMember, addedRoles);
 		}
 	},
+
+	handleRolesAdded(guildMember: GuildMember, roles: Collection<Snowflake, Role>) {
+		roles.each(role => {
+			switch (role.name) {
+				case constants.DISCORD_ROLE_GUEST_PASS:
+					updateNotionGuestPassDatabase(guildMember.user.tag, true);	
+					break;
+				case constants.DISCORD_ROLE_DEVELOPERS_GUILD:
+					module.exports.sendGuildWelcomeMessage(guildMember, role);
+					break;
+			}
+		})
+	},
+
+	handleRolesRemoved(guildMember: GuildMember, roles: Collection<Snowflake, Role>) {
+		roles.each(role => {
+			switch (role.name) {
+				case constants.DISCORD_ROLE_GUEST_PASS:
+					updateNotionGuestPassDatabase(guildMember.user.tag, false);
+					break;
+			}
+		})
+	},
+
+	async sendGuildWelcomeMessage(guildMember: GuildMember, role: Role) {
+		const response = await notion.databases.query({
+			database_id: process.env.DEV_GUILD_PROJECTS_DATABASE_ID,
+			filter: {
+				property: 'Status',
+				select: {
+					equals: 'Active'
+				}
+			}
+		});
+
+		let message = '';
+		
+		
+
+		console.log(response)
+
+	}
+
 };

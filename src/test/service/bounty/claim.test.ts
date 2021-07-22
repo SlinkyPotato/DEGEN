6fcebd9fdda013ce450ec7bcc40b255fd1f4ba67
Brian <patinobrian@gmail.com>
2021-07-22 16:22:29 -0400

    mock discord client api error

--- src/test/service/bounty/claim.test.ts
+++ src/test/service/bounty/claim.test.ts
@@ -1,14 +1,15 @@
 import * as chai from 'chai';
 import claim from '../../../app/service/bounty/claim';
 import * as sinon from 'sinon';
-import db from '../../../app/db/db';
-import { MongoClient, MongoError } from 'mongodb';
+import { MongoClient } from 'mongodb';
 import serviceUtils from '../../../app/service/ServiceUtils';
+import { DiscordAPIError } from 'discord.js';
 
 const assert = chai.assert;
 
 describe('BountyClaimTest', () => {
 	let ctx;
+	let serviceUtilsStub;
 
 	beforeEach(() => {
 		ctx = {
@@ -24,8 +25,8 @@ describe('BountyClaimTest', () => {
 			send: (message: string) => { return message; },
 		};
 
-		const stub = sinon.stub(serviceUtils, 'getGuildMember');
-		stub.returns({ send: (message) => { return message; } });
+		serviceUtilsStub = sinon.stub(serviceUtils, 'getGuildMember');
+		serviceUtilsStub.returns({ send: (message) => { return message; } });
 	});
 
 	afterEach(() => {
@@ -34,13 +35,31 @@ describe('BountyClaimTest', () => {
 
 	describe('Parameter Validation', () => {
 
-		it('should be invalid bountyId', async function() {
+		it('should be invalid bountyId for null', async function() {
 			ctx.options.claim['bounty-id'] = null;
 			const result = await claim(ctx);
 			assert.equal(result, '<@567865362541182987>\n' +
 			'Please enter a valid bounty hash ID: \n' +
 			' - can be found on bountyboard website\n' +
 			' - https://bankless.community');
+		});
+
+		it('should be invalid bountyId full special character', async function() {
+			ctx.options.claim['bounty-id'] = '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$';
+			const result = await claim(ctx);
+			assert.equal(result, '<@567865362541182987>\n' +
+				'Please enter a valid bounty hash ID: \n' +
+				' - can be found on bountyboard website\n' +
+				' - https://bankless.community');
+		});
+
+		it('should be invalid bountyId full negative numbers', async function() {
+			ctx.options.claim['bounty-id'] = '-10005';
+			const result = await claim(ctx);
+			assert.equal(result, '<@567865362541182987>\n' +
+				'Please enter a valid bounty hash ID: \n' +
+				' - can be found on bountyboard website\n' +
+				' - https://bankless.community');
 		});
 
 	});
@@ -54,6 +73,19 @@ describe('BountyClaimTest', () => {
 			const result = await (await claim(ctx));
 
 			assert.equal(result, 'Sorry something is not working, our devs are looking into it.');
+		});
+
+		it('should be client api error', async () => {
+			serviceUtilsStub.restore();
+
+			const mock = sinon.mock(serviceUtils);
+			mock.expects('getGuildMember').once().throws(new DiscordAPIError('', new Error('Mock Discord API Error'), 'GET', 405));
+
+			try {
+				await (await claim(ctx));
+			} catch (e) {
+				assert.equal(e.message, 'Mock Discord API Error');
+			}
 		});
 
 	});

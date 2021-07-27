9908f1ce7e44522ce7b401d506679f45ca4a4a5e
boris <a@b.c>
2021-07-27 19:12:39 +0800

    basic poll with reaction and user collection

--- src/app/service/HttpServer.ts
+++ src/app/service/HttpServer.ts
@@ -1,5 +1,3 @@
-// const { Client } = require("discord.js");
-// const Config = require("./config");
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
@@ -9,7 +7,7 @@ import constants from './../constants';
 
 export default async (client: DiscordClient): Promise<void> => {
   console.log('starting http server...');
-  var scope = "nothing bro"
+  var scoap = "nothing bro"
   const app = express()
   const channel = await client.channels.fetch(constants.SCOAP_SQUAD_CHANNEL_ID) as TextChannel;
 
@@ -35,23 +33,35 @@ export default async (client: DiscordClient): Promise<void> => {
     res.send("Hello World")
   })
 
-  app.get("/scope", (req, res) => {
-    res.send(scope)
+  app.get("/scoap", (req, res) => {
+    res.send(scoap)
   })
 
-  app.post("/scope", (req, res) => {
-    if (!req.body.scope) return res.status(400).send("missing scope")
-    scope = req.body.scope 
-    console.log('post request received, form data: ', scope )
-    const message = 'post request received, form data: ' + scope 
+  app.post("/scoap", async (req, res) => {
+    if (!req.body.scoap) return res.status(400).send("missing scoap")
+    scoap = req.body.scoap 
+    console.log('post request received, form data: ', scoap )
+    const message = 'post request received, form data: ' + scoap 
 
     if (!channel) return;
 
     // Using a type guard to narrow down the correct type
     if (!((channel): channel is TextChannel => channel.type === 'text')(channel)) return;
 
-    channel.send(message);
-    // channel.send("what you want to send to that channel");
+    const pollMessage = await channel.send(message);
+    await pollMessage.react(`✅`);
+    await pollMessage.react(`⛔`);
+    // Create a reaction collector
+    const filter = (reaction) => reaction.emoji.name === '✅';
+    const collector = pollMessage.createReactionCollector(filter, { time: 30000 });
+    collector.on('collect', async (reaction, user) => {
+    console.log(`Collected ${reaction.emoji.name} from user ${user.id}`)
+    let msg = `Collected ${reaction.emoji.name} from user ${user.username}`
+    const pollMessage = await channel.send(msg);
+    });
+
+    collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+
     
     res.send("completed")
   })
@@ -62,53 +72,3 @@ export default async (client: DiscordClient): Promise<void> => {
 
 }
 
-
-
-// // ----
-// // const client = new Client();
-// var motd = "nothing bro"
-
-// const app = express()
-
-// //========== EXPRESS HTTP SERVER
-// app.use(helmet());
-// app.use(cors());
-
-// app.use(express.json());
-// app.use(express.urlencoded());
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World")
-// })
-
-// app.post("/motd", (req, res) => {
-//   if (!req.body.motd) return res.status(400).send("missing motd")
-//   motd = req.body.motd 
-
-//   res.send("completed")
-// })
-
-// app.listen(5000, () => {
-//   console.log("Listening at http://localhost:5000")
-// })
-
-
-// //========== DISCORD BOT
-// client.on("ready", () => {
-//   console.log("Bot Listening")
-// })
-
-// client.on("message", (message) => {
-//   if (message.author.bot || !message.guild) return;
-
-//   // command handler
-//   if (!message.content.toLowerCase().startsWith(Config.prefix)) return;
-
-//   const [command, ...args] = message.content.slice(Config.prefix.length).split(/\s+/g);
-  
-//   if (command == "motd") {
-//     message.channel.send(motd)
-//   }
-// })
-
-// client.login(Config.token)

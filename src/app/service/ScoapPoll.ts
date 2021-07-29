7c28d2958396b97f7a1cb8cbbe06d2c09a784baf
boris <a@b.c>
2021-07-29 22:46:18 +0800

    embed update based on reaction capture

--- src/app/service/ScoapPoll.ts
+++ src/app/service/ScoapPoll.ts
@@ -74,6 +74,64 @@ const scoapEmbed = {
 	},
 };
 
+const updateEmbedFields = (emoji_name, embedFields) => {
+
+	
+	switch (true){
+      case emoji_name === constants.EMOJIS.one:
+      	{
+      	let st = embedFields[1].name.match(/\(([^)]+)\)/)[1];
+      	let current = st.split('/')[0];
+      	let target = st.split('/')[1];
+      	let newCurrent = parseInt(current) + 1;
+      	let percent = Math.round(100/parseInt(target)*newCurrent);
+      	let updateStr = `${percent}%(${newCurrent}/${target})`;
+      	embedFields[1] =  {
+				name: updateStr,
+				value: '\u200b',
+				inline: true,
+		};
+        return (embedFields);
+	    };
+	       
+      case emoji_name === constants.EMOJIS.two:
+      	{
+      	let st = embedFields[4].name.match(/\(([^)]+)\)/)[1];
+      	let current = st.split('/')[0];
+      	let target = st.split('/')[1];
+      	let newCurrent = parseInt(current) + 1;
+      	let percent = Math.round(100/parseInt(target)*newCurrent);
+      	let updateStr = `${percent}%(${newCurrent}/${target})`;
+      	embedFields[4] =  {
+				name: updateStr,
+				value: '\u200b',
+				inline: true,
+		};
+        return (embedFields);
+	    };
+
+       case emoji_name === constants.EMOJIS.three: 
+       	{
+      	let st = embedFields[7].name.match(/\(([^)]+)\)/)[1];
+      	let current = st.split('/')[0];
+      	let target = st.split('/')[1];
+      	let newCurrent = parseInt(current) + 1;
+      	let percent = Math.round(100/parseInt(target)*newCurrent);
+      	let updateStr = `${percent}%(${newCurrent}/${target})`;
+        embedFields[7] =  {
+				name: updateStr,
+				value: '\u200b',
+				inline: true,
+		};
+        return (embedFields);
+	    };
+
+      default:
+        console.log('This should never happen!');
+    }
+
+};
+
 //Note: How to do correct type definition for request?
 export default async (channel: TextChannel, request: any): Promise<any> => {
 	console.log(' here is the request body ', request.body)
@@ -84,28 +142,30 @@ export default async (channel: TextChannel, request: any): Promise<any> => {
 	await embedMessage.react(constants.EMOJIS.one);
 	await embedMessage.react(constants.EMOJIS.two);
 	await embedMessage.react(constants.EMOJIS.three);
-
-	// await pollMessage.react(String.fromCodePoint(0x1F354));
-	// await pollMessage.react(String.fromCodePoint(0x0031));
-	
-	
 	
 	// const four = client.emojis.cache.find(emoji => emoji.name === "like");
 	// const four = client.emojis.find(emoji => emoji.name === "like");
-	
-
-	// await pollMessage.react(four);
 
 	// Create a reaction collector
-	// const filter = (reaction) => reaction.emoji.name === '✅';
-	// const collector = pollMessage.createReactionCollector(filter, { time: 10000 });
-	// collector.on('collect', async (reaction, user) => {
-	//   console.log(`Collected ${reaction.emoji.name} from user ${user.id}`)
-	//   let msg = `Collected ${reaction.emoji.name} from user ${user.username}`
-	//   const pollMessage = await channel.send(msg);
-	// });
+	const filter = (reaction) => reaction.emoji.name === constants.EMOJIS.one || reaction.emoji.name === constants.EMOJIS.two || reaction.emoji.name === constants.EMOJIS.three;
+	const collector = embedMessage.createReactionCollector(filter, /*{ time: 10000 }*/ );
+	collector.on('collect', async (reaction, user) => {
 
-	// collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+		// let msg = `Collected ${reaction.emoji.name} from user ${user.username}`
+		// immutably copy embed's fields to new obj
+		var embedFields = Object.assign([], embedMessage.embeds[0].fields);
+		console.log('3', embedFields);
+		// validate before running this!
+		let updatedEmbedFields = updateEmbedFields(reaction.emoji.name, embedFields)
+		console.log('4', updatedEmbedFields);
+		scoapEmbed.fields = updatedEmbedFields
+		embedMessage.edit({ embed: scoapEmbed });
+		console.log(`Collected ${reaction.emoji.name} from user ${user.id}`)
+	});
+
+	collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+	
+
 	return (request.body);
 	
 }

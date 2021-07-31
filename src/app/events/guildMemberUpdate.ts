ba994078e19ad5f5c4fb715929017a6c32ff0336
Austin Mitchem <austinmitchem@outlook.com>
2021-07-31 08:00:41 -0500

    send welcome message on adding developers role

--- src/app/events/guildMemberUpdate.ts
+++ src/app/events/guildMemberUpdate.ts
@@ -2,11 +2,11 @@
  * Handler for Discord event `guildMemberUpdate`.
  */
 
-import { Collection, GuildMember, Role, Snowflake } from 'discord.js';
+import { Collection, GuildMember, Role, Snowflake, MessageEmbed } from 'discord.js';
 import { Client } from '@notionhq/client';
 import { updateNotionGuestPassDatabase } from '../service/GuestPassService';
 import constants from '../constants';
-import { GuildMember } from 'discord.js';
+import notionPageRefs from '../api/notion/NotionGuildPages';
 
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
@@ -15,20 +15,26 @@ module.exports = {
 	once: false,
 
 	async execute(oldMember: GuildMember, newMember: GuildMember) {
-		console.log('Guild member updated')
+		console.debug('Guild member updated')
 		const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
 		if (removedRoles.size > 0) { 
-			console.log(`The roles ${removedRoles.map(r => r.name)} were removed from ${oldMember.displayName}.`);
+			console.debug(`The roles ${removedRoles.map(r => r.name)} were removed from ${oldMember.displayName}.`);
 			module.exports.handleRolesRemoved(newMember, removedRoles);
 		}
 
 		const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
 		if (addedRoles.size > 0) {
-			console.log(`The roles ${addedRoles.map(r => r.name)} were added to ${oldMember.displayName}.`);
+			console.debug(`The roles ${addedRoles.map(r => r.name)} were added to ${oldMember.displayName}.`);
 			module.exports.handleRolesAdded(newMember, addedRoles);
 		}
 	},
 
+	/**
+	 * Handler for when roles are added to a member.
+	 * 
+	 * @param guildMember member that roles were added to
+	 * @param roles roles that were added to member
+	 */
 	handleRolesAdded(guildMember: GuildMember, roles: Collection<Snowflake, Role>) {
 		roles.each(role => {
 			switch (role.name) {
@@ -40,12 +46,18 @@ module.exports = {
 					}
 					break;
 				case constants.DISCORD_ROLE_DEVELOPERS_GUILD:
-					module.exports.sendGuildWelcomeMessage(guildMember, role);
+					module.exports.sendGuildWelcomeMessage(guildMember);
 					break;
 			}
 		})
 	},
 
+	/**
+	 * Handler for when roles are removed from a member.
+	 * 
+	 * @param guildMember member that roles were removed from
+	 * @param roles roles that were removed from member
+	 */
 	handleRolesRemoved(guildMember: GuildMember, roles: Collection<Snowflake, Role>) {
 		roles.each(role => {
 			switch (role.name) {
@@ -56,7 +68,12 @@ module.exports = {
 		})
 	},
 
-	async sendGuildWelcomeMessage(guildMember: GuildMember, role: Role) {
+	/**
+	 * Sends custom welcome message on adding Developer's Guild role
+	 * 
+	 * @param guildMember member that has added Developer's Guild role
+	 */
+	async sendGuildWelcomeMessage(guildMember: GuildMember) {
 		const response = await notion.databases.query({
 			database_id: process.env.DEV_GUILD_PROJECTS_DATABASE_ID,
 			filter: {
@@ -68,11 +85,61 @@ module.exports = {
 		});
 
 		let message = '';
-		
-		
+		message += 'Here are some resources to help you get started:\n'
+		message += `[Developer\'s Guild Notion](${notionPageRefs.developers})\n`
+		message += '[Bankless DAO Github](https://github.com/BanklessDAO)\n'
+		message += '\n'
+		message += 'Here are the projects we are working on:\n'
 
-		console.log(response)
+		response.results.forEach(result => {
+			let project = result['properties']['Project']['title'][0]['text']['content'];
+			let page = result['url']
+			if (project && page) {
+				message += `**[${project}](${page})**\n`;
+			} else if (project) {
+				message += `**${project}**\n`;
+			} else return;
 
+			let description = result['properties']['Description']['rich_text'][0]['text']['content'];
+			if (description) {
+				message += `${description}\n`;
+			}
+
+			let techStack = result['properties']['Tech Stack']['multi_select'];
+			if (techStack) {
+				message += `Tech Stack: `
+				let length = techStack.length;
+				techStack.forEach((element, index) => {
+					message += element['name']
+					if (index < length - 1) {
+						message += ', '
+					} else {
+						message += '\n'
+					}
+				});
+			}
+
+			let githubRepo = result['properties']['Github']['rich_text'][0]['text']['content'];
+			let githubLink = result['properties']['Github']['rich_text'][0]['text']['link']['url']
+			if (githubRepo && githubLink) {
+				message += `Github: [${githubRepo}](${githubLink})\n`;
+			}
+
+			let discordChannel = result['properties']['Discord Channel']['rich_text'][0]['text']['content'];
+			let discordLink = result['properties']['Discord Channel']['rich_text'][0]['text']['link']['url']
+			if (discordChannel && discordLink) {
+				message += `Discord: [${discordChannel}](${discordLink})\n`;
+			}
+
+			message += '\n'
+		})
+		
+		const embed = new MessageEmbed()
+        	.setTitle('Welcome to the Developer\'s Guild!')
+        	.setColor(0xF1BD1B)
+        	.setDescription(message)
+
+		guildMember.send(embed);
 	}
 
 };

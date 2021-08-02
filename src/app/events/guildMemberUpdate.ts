1cfabb4688f44183a4feb4c489fbaba8fd0e242e
nonsensecodes <jjoshua@nonsensecodes.io>
2021-08-01 21:06:31 -0700

    refactored to modularize welcome mats. still needs work.

--- src/app/events/guildMemberUpdate.ts
+++ src/app/events/guildMemberUpdate.ts
@@ -2,13 +2,10 @@
  * Handler for Discord event `guildMemberUpdate`.
  */
 
-import { Collection, GuildMember, Role, Snowflake, MessageEmbed } from 'discord.js';
-import { Client } from '@notionhq/client';
+import { Collection, GuildMember, Role, Snowflake } from 'discord.js';
 import { updateNotionGuestPassDatabase } from '../service/GuestPassService';
 import constants from '../constants';
-import notionPageRefs from '../api/notion/NotionGuildPages';
-
-const notion = new Client({ auth: process.env.NOTION_TOKEN });
+import sendGuildWelcomeMessage from './welcomeMats/devGuild';
 
 module.exports = {
 	name: 'guildMemberUpdate',
@@ -42,14 +39,18 @@ module.exports = {
 					try {
 						updateNotionGuestPassDatabase(guildMember.user.tag, true);	
 					} catch (e) {
-						console.error(e)
+						console.error(e);
 					}
 					break;
 				case constants.DISCORD_ROLE_DEVELOPERS_GUILD:
-					module.exports.sendGuildWelcomeMessage(guildMember);
+					try {
+						sendGuildWelcomeMessage(guildMember);
+					} catch(e) {
+						console.error(e);
+					}
 					break;
 			}
-		})
+		});
 	},
 
 	/**
@@ -62,84 +63,8 @@ module.exports = {
 		roles.each(role => {
 			switch (role.name) {
 				case constants.DISCORD_ROLE_GUEST_PASS:
-					updateNotionGuestPassDatabase(guildMember.user.tag, false);
-					break;
-			}
-		})
-	},
-
-	/**
-	 * Sends custom welcome message on adding Developer's Guild role
-	 * 
-	 * @param guildMember member that has added Developer's Guild role
-	 */
-	async sendGuildWelcomeMessage(guildMember: GuildMember) {
-		const response = await notion.databases.query({
-			database_id: process.env.DEV_GUILD_PROJECTS_DATABASE_ID,
-			filter: {
-				property: 'Status',
-				select: {
-					equals: 'Active'
-				}
+					return updateNotionGuestPassDatabase(guildMember.user.tag, false);
 			}
 		});
-
-		let message = '';
-		message += 'Here are some resources to help you get started:\n'
-		message += `[Developer\'s Guild Notion](${notionPageRefs.developers})\n`
-		message += '[Bankless DAO Github](https://github.com/BanklessDAO)\n'
-		message += '\n'
-		message += 'Here are the projects we are working on:\n'
-
-		response.results.forEach(result => {
-			let project = result['properties']['Project']['title'][0]['text']['content'];
-			let page = result['url']
-			if (project && page) {
-				message += `**[${project}](${page})**\n`;
-			} else if (project) {
-				message += `**${project}**\n`;
-			} else return;
-
-			let description = result['properties']['Description']['rich_text'][0]['text']['content'];
-			if (description) {
-				message += `${description}\n`;
-			}
-
-			let techStack = result['properties']['Tech Stack']['multi_select'];
-			if (techStack) {
-				message += `Tech Stack: `
-				let length = techStack.length;
-				techStack.forEach((element, index) => {
-					message += element['name']
-					if (index < length - 1) {
-						message += ', '
-					} else {
-						message += '\n'
-					}
-				});
-			}
-
-			let githubRepo = result['properties']['Github']['rich_text'][0]['text']['content'];
-			let githubLink = result['properties']['Github']['rich_text'][0]['text']['link']['url']
-			if (githubRepo && githubLink) {
-				message += `Github: [${githubRepo}](${githubLink})\n`;
-			}
-
-			let discordChannel = result['properties']['Discord Channel']['rich_text'][0]['text']['content'];
-			let discordLink = result['properties']['Discord Channel']['rich_text'][0]['text']['link']['url']
-			if (discordChannel && discordLink) {
-				message += `Discord: [${discordChannel}](${discordLink})\n`;
-			}
-
-			message += '\n'
-		})
-		
-		const embed = new MessageEmbed()
-        	.setTitle('Welcome to the Developer\'s Guild!')
-        	.setColor(0xF1BD1B)
-        	.setDescription(message)
-
-		guildMember.send(embed);
-	}
-
+	},
 };

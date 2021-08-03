e6543e6e830e1cc808d818208560bde70792d83a
Brian <patinobrian@gmail.com>
2021-08-03 17:45:11 -0400

    notify of bounty submission

--- src/app/service/bounty/SubmitBounty.ts
+++ src/app/service/bounty/SubmitBounty.ts
@@ -61,6 +61,10 @@ export const submitBountyForValidId = async (guildMember: GuildMember,
 
 	console.log(`${bountyId} bounty submitted by ${guildMember.user.tag}`);
 	await submitBountyMessage(guildMember, dbBountyResult.discordMessageId, message);
+	
+	const bountyUrl = constants.BOUNTY_BOARD_URL + dbBountyResult._id;
+	const createdByUser: GuildMember = guildMember.guild.member(dbBountyResult.createdBy.discordId);
+	await createdByUser.send(`Hello <@${createdByUser.user.id}>! Bankless DAO user <@${guildMember.user.id}> has finished the bounty ${bountyUrl}. Please reach out to them to check.`);
 
 	return guildMember.send(`<@${guildMember.user.id}> Bounty in review! Look out for a follow up message from <@${dbBountyResult.createdBy.id}>`);
 };

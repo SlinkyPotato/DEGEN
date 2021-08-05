d2a4542ee79bb9e8e5af667365a6b4a5f745b426
boris <40813246+tikisailor@users.noreply.github.com>
2021-08-05 13:39:16 +0800

    done cleanup

--- src/app/service/ScoapFastifyServer.ts
+++ src/app/service/ScoapFastifyServer.ts
@@ -18,42 +18,42 @@ server.register(helmet);
 server.register(cors, { origin: constants.SCOAP_HTTP_SERVER_CORS_WHITELIST });
 
 const opts: RouteShorthandOptions = {
-    schema: {
-        response: {
-            200: {
-                type: 'object',
-                properties: {
-                    response: {
-                        type: 'string',
-                    },
-                },
-            },
-        },
-    },
+	schema: {
+		response: {
+			200: {
+				type: 'object',
+				properties: {
+					response: {
+						type: 'string',
+					},
+				},
+			},
+		},
+	},
 };
 
 // route definitions // (request, reply)
 server.post('/scoap', opts, async (request) => {
-    // console.log(typeof request.body);
-    const channel = (await client.channels.fetch(
-        constants.SCOAP_SQUAD_CHANNEL_ID
-    )) as TextChannel;
-    // const requestBody: object = request.body;
-    const formData = await ScoapPoll(channel, request);
-    return formData;
+	// console.log(typeof request.body);
+	const channel = (await client.channels.fetch(
+		constants.SCOAP_SQUAD_CHANNEL_ID
+	)) as TextChannel;
+	// const requestBody: object = request.body;
+	const formData = await ScoapPoll(channel, request);
+	return formData;
 });
 
 export default async (): Promise<void> => {
-    try {
-        await server.listen(constants.SCOAP_HTTP_SERVER_PORT);
-        const address = server.server.address();
-        // const port = typeof address === 'string' ? address : address?.port;
+	try {
+		await server.listen(constants.SCOAP_HTTP_SERVER_PORT);
+		const address = server.server.address();
+		// const port = typeof address === 'string' ? address : address?.port;
 
-        console.log('fastify listening on: ', address);
+		console.log('fastify listening on: ', address);
 
-        // return server
-    } catch (err) {
-        server.log.error(err);
-        process.exit(1);
-    }
+		// return server
+	} catch (err) {
+		server.log.error(err);
+		process.exit(1);
+	}
 };
--- src/app/service/ScoapPoll.ts
+++ src/app/service/ScoapPoll.ts
@@ -5,212 +5,225 @@ import constants from './../constants';
 import { ScoapEmbed, Vote, VoteRecord } from './ScoapClasses';
 
 const scoapEmbedTemplate = {
-    color: 0x0099ff,
-    title: 'SCOAP SQUAD - NEW WEBSITE',
-    url: 'https://www.bankless.community',
-    author: {
-        name: 'posted by user Tiki',
-        url: 'https://www.bankless.community',
-    },
-    description: 'Project Summary',
-    fields: [
-        {
-            name: `${constants.EMOJIS.one} PM`,
-            value: '\u200b',
-            inline: true,
-        },
+	color: 0x0099ff,
+	title: 'SCOAP SQUAD - NEW WEBSITE',
+	url: 'https://www.bankless.community',
+	author: {
+		name: 'posted by user Tiki',
+		url: 'https://www.bankless.community',
+	},
+	description: 'Project Summary',
+	fields: [
+		{
+			name: `${constants.EMOJIS.one} PM`,
+			value: '\u200b',
+			inline: true,
+		},
 
-        {
-            name: '0 % (0/1)',
-            value: '\u200b',
-            inline: true,
-        },
-        {
-            name: '\u200b',
-            value: '\u200b',
-            inline: false,
-        },
-        {
-            name: `${constants.EMOJIS.two} DEV`,
-            value: 'JS, Web3',
-            inline: true,
-        },
-        {
-            name: '0% (0/3)',
-            value: '\u200b',
-            inline: true,
-        },
-        {
-            name: '\u200b',
-            value: '\u200b',
-            inline: false,
-        },
-        {
-            name: `${constants.EMOJIS.three} UI`,
-            value: 'Chakra',
-            inline: true,
-        },
-        {
-            name: '0% (0/2)',
-            value: '\u200b',
-            inline: true,
-        },
-    ],
-    timestamp: new Date(),
-    footer: {
-        text: 'You may select only one option in this poll',
-    },
+		{
+			name: '0 % (0/1)',
+			value: '\u200b',
+			inline: true,
+		},
+		{
+			name: '\u200b',
+			value: '\u200b',
+			inline: false,
+		},
+		{
+			name: `${constants.EMOJIS.two} DEV`,
+			value: 'JS, Web3',
+			inline: true,
+		},
+		{
+			name: '0% (0/3)',
+			value: '\u200b',
+			inline: true,
+		},
+		{
+			name: '\u200b',
+			value: '\u200b',
+			inline: false,
+		},
+		{
+			name: `${constants.EMOJIS.three} UI`,
+			value: 'Chakra',
+			inline: true,
+		},
+		{
+			name: '0% (0/2)',
+			value: '\u200b',
+			inline: true,
+		},
+	],
+	timestamp: new Date(),
+	footer: {
+		text: 'You may select only one option in this poll',
+	},
 };
 
-
 const removeReaction = async (message, user_id, emoji, choice_valid) => {
-    const userReactions = message.reactions.cache.filter((reaction) =>
-        reaction.users.cache.has(user_id));
-    try {
-        for (const reac of userReactions.values()) {
-            if (choice_valid === true) {
-                // the selected choice is available, only remove choices that don't match current choice
-                if (reac.emoji.name !== emoji) {
-                    await reac.users.remove(user_id);
-                    // console.log('reaction removed by bot, case true');
-                    break;
-                }
-            } else if (choice_valid === false) {
-                // the selected choice is not available, only remove choices that do match current choice
-                if (reac.emoji.name === emoji) {
-                    await reac.users.remove(user_id);
-                    // console.log('reaction removed by bot, case false');
-                    break;
-                }
-            }
-        }
-    } catch (error) {
-        console.error('Failed to remove reactions.');
-    }
+	const userReactions = message.reactions.cache.filter((reaction) =>
+		reaction.users.cache.has(user_id)
+	);
+	try {
+		for (const reac of userReactions.values()) {
+			if (choice_valid === true) {
+				// the selected choice is available, only remove choices that don't match current choice
+				if (reac.emoji.name !== emoji) {
+					await reac.users.remove(user_id);
+					// console.log('reaction removed by bot, case true');
+					break;
+				}
+			} else if (choice_valid === false) {
+				// the selected choice is not available, only remove choices that do match current choice
+				if (reac.emoji.name === emoji) {
+					await reac.users.remove(user_id);
+					// console.log('reaction removed by bot, case false');
+					break;
+				}
+			}
+		}
+	} catch (error) {
+		console.error('Failed to remove reactions.');
+	}
 };
 
 const emojiValid = (emoji, valid_emoji_array) => {
-    return valid_emoji_array.includes(emoji);
+	return valid_emoji_array.includes(emoji);
 };
 
 const validateChoice = (emoji, totals, required) => {
-    if (totals[emoji] < required[emoji]) {
-        // console.log('selected choice available');
-        return true;
-    } else {
-        // console.log('selected choice invalid');
-        return false;
-    }
+	if (totals[emoji] < required[emoji]) {
+		// console.log('selected choice available');
+		return true;
+	} else {
+		// console.log('selected choice invalid');
+		return false;
+	}
 };
 
 // Note: How to do correct type definition for request?
 export default async (channel: TextChannel, request: any): Promise<any> => {
-    // console.log(' here is the request body ', request.body.scoap);
-    const cloneDeep = require('lodash.clonedeep');
-    const deepEmbed = cloneDeep(scoapEmbedTemplate);
+	// console.log(' here is the request body ', request.body.scoap);
+	const cloneDeep = require('lodash.clonedeep');
+	const deepEmbed = cloneDeep(scoapEmbedTemplate);
 
-    const votableEmojiArray = [
-        constants.EMOJIS.one,
-        constants.EMOJIS.two,
-        constants.EMOJIS.three,
-    ];
+	const votableEmojiArray = [
+		constants.EMOJIS.one,
+		constants.EMOJIS.two,
+		constants.EMOJIS.three,
+	];
 
-    const validEmojiArray = votableEmojiArray.push(constants.EMOJIS.cross_mark);
+	const validEmojiArray = votableEmojiArray.push(constants.EMOJIS.cross_mark);
 
-    const voteRecord = new VoteRecord();
+	const voteRecord = new VoteRecord();
 
-    const scoapEmbed = new ScoapEmbed(deepEmbed, '749152252966469668');
+	const scoapEmbed = new ScoapEmbed(deepEmbed, '749152252966469668');
 
-    const embedMessage = await channel.send({ embed: scoapEmbed.getEmbed() });
+	const embedMessage = await channel.send({ embed: scoapEmbed.getEmbed() });
 
-    await embedMessage.react(constants.EMOJIS.one);
-    await embedMessage.react(constants.EMOJIS.two);
-    await embedMessage.react(constants.EMOJIS.three);
-    await embedMessage.react(constants.EMOJIS.cross_mark);
+	await embedMessage.react(constants.EMOJIS.one);
+	await embedMessage.react(constants.EMOJIS.two);
+	await embedMessage.react(constants.EMOJIS.three);
+	await embedMessage.react(constants.EMOJIS.cross_mark);
 
-    const filter = (reaction) => {
-        const emoji_valid = emojiValid(reaction.emoji.name, validEmojiArray);
-        // console.log('emoji valid? ', emoji_valid);
-        return emoji_valid;
-    };
+	const filter = (reaction) => {
+		const emoji_valid = emojiValid(reaction.emoji.name, validEmojiArray);
+		// console.log('emoji valid? ', emoji_valid);
+		return emoji_valid;
+	};
 
-    const collector = embedMessage.createReactionCollector(filter, { dispose: true });
+	const collector = embedMessage.createReactionCollector(filter, {
+		dispose: true,
+	});
 
-    collector.on('collect', async (reaction, user) => {
-        const choiceValid = validateChoice(
-            reaction.emoji.name,
-            voteRecord.getEmoteTotals(),
-            voteRecord.getEmoteRequired(),
-        );
+	collector.on('collect', async (reaction, user) => {
+		const choiceValid = validateChoice(
+			reaction.emoji.name,
+			voteRecord.getEmoteTotals(),
+			voteRecord.getEmoteRequired()
+		);
 
-        if (emojiValid(reaction.emoji.name, votableEmojiArray) === true) {
+		if (emojiValid(reaction.emoji.name, votableEmojiArray) === true) {
+			switch (true) {
+				case choiceValid === true: {
+					const vote = new Vote(
+						user.id,
+						reaction.emoji.name,
+						voteRecord.getUserVoteLedger()
+					);
+					voteRecord.update(vote);
 
-            switch (true) {
-                case choiceValid === true: {
-                    const vote = new Vote(user.id, reaction.emoji.name, voteRecord.getUserVoteLedger());
-                    voteRecord.update(vote);
+					if (vote.getType() === 'CHANGEVOTE') {
+						await removeReaction(
+							embedMessage,
+							vote.getUserId(),
+							vote.getEmoji(),
+							choiceValid
+						);
+					}
 
-                    if (vote.getType() === 'CHANGEVOTE') {
-                        await removeReaction(
-                            embedMessage,
-                            vote.getUserId(),
-                            vote.getEmoji(),
-                            choiceValid,
-                        );
-                    }
+					for (const key in voteRecord.getProgressStrings()) {
+						// console.log('key ', key, ' value ', voteRecord.getProgressStrings()[key]);
+						scoapEmbed.updateProgressString(
+							key,
+							voteRecord.getProgressStrings()[key]
+						);
+					}
+					embedMessage.edit({ embed: scoapEmbed.getEmbed() });
 
-                    for (const key in voteRecord.getProgressStrings()) {
-                        // console.log('key ', key, ' value ', voteRecord.getProgressStrings()[key]);
-                        scoapEmbed.updateProgressString(key, voteRecord.getProgressStrings()[key]);
-                    }
-                    embedMessage.edit({ embed: scoapEmbed.getEmbed() });
+					break;
+				}
+				case choiceValid === false: {
+					await removeReaction(
+						embedMessage,
+						user.id,
+						reaction.emoji.name,
+						choiceValid
+					);
+					break;
+				}
+			}
+		} else {
+			console.log(reaction.emoji.name);
+			// handle delete
+			// handle add
+			// handle edit
+		}
 
-                    break;
-                }
-                case choiceValid === false: {
-                    await removeReaction(
-                        embedMessage,
-                        user.id,
-                        reaction.emoji.name,
-                        choiceValid,
-                    );
-                    break;
-                }
-            }
+		// console.log(`Collected ${reaction.emoji.name} from user ${user.id}`);
+		// console.log('current state: \n emote total ', voteRecord.getEmoteTotals());
+		// console.log('current state: \n emote total ', voteRecord.getEmoteRequired());
+		// console.log('current state: \n user vote record ', voteRecord.getUserVoteLedger());
+	});
 
-        } else {
+	collector.on('end', (collected) =>
+		console.log(`Collected ${collected.size} items`)
+	);
 
-            console.log(reaction.emoji.name);
-            // handle delete
-            // handle add
-            // handle edit
-        }
-        
-        // console.log(`Collected ${reaction.emoji.name} from user ${user.id}`);
-        // console.log('current state: \n emote total ', voteRecord.getEmoteTotals());
-        // console.log('current state: \n emote total ', voteRecord.getEmoteRequired());
-        // console.log('current state: \n user vote record ', voteRecord.getUserVoteLedger());
-    });
+	collector.on('remove', async (reaction, user) => {
+		const vote = new Vote(
+			user.id,
+			reaction.emoji.name,
+			voteRecord.getUserVoteLedger()
+		);
+		if (vote.getType() === 'UNVOTE') {
+			voteRecord.update(vote);
+			for (const key in voteRecord.getProgressStrings()) {
+				// console.log('key ', key, ' value ', voteRecord.getProgressStrings()[key]);
+				scoapEmbed.updateProgressString(
+					key,
+					voteRecord.getProgressStrings()[key]
+				);
+			}
+			embedMessage.edit({ embed: scoapEmbed.getEmbed() });
 
-    collector.on('end', (collected) => console.log(`Collected ${collected.size} items`));
-
-
-    collector.on('remove', async (reaction, user) => {
-        const vote = new Vote(user.id, reaction.emoji.name, voteRecord.getUserVoteLedger());
-        if (vote.getType() === 'UNVOTE') {
-            voteRecord.update(vote);
-            for (const key in voteRecord.getProgressStrings()) {
-                // console.log('key ', key, ' value ', voteRecord.getProgressStrings()[key]);
-                scoapEmbed.updateProgressString(key, voteRecord.getProgressStrings()[key]);
-            }
-            embedMessage.edit({ embed: scoapEmbed.getEmbed() });
-
-            // console.log(`Collected ${reaction.emoji.name} from user ${user.id}`);
-            // console.log('current state: \n emote total ', voteRecord.getEmoteTotals());
-            // console.log('current state: \n emote total ', voteRecord.getEmoteRequired());
-            // console.log('current state: \n user vote record ', voteRecord.getUserVoteLedger());
-        }
-        
-    });
-    return request.body;
+			// console.log(`Collected ${reaction.emoji.name} from user ${user.id}`);
+			// console.log('current state: \n emote total ', voteRecord.getEmoteTotals());
+			// console.log('current state: \n emote total ', voteRecord.getEmoteRequired());
+			// console.log('current state: \n user vote record ', voteRecord.getUserVoteLedger());
+		}
+	});
+	return request.body;
 };

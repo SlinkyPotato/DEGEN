8b1951cc57ad3fca894abcb4d5b13fdd0bc4df74
Austin Mitchem <austinmitchem@outlook.com>
2021-08-16 17:31:43 -0500

    fix type error

--- src/app/events/guildMemberUpdate.ts
+++ src/app/events/guildMemberUpdate.ts
@@ -17,55 +17,55 @@ module.exports = {
 		const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
 		if (removedRoles.size > 0) { 
 			console.debug(`The roles ${removedRoles.map(r => r.name)} were removed from ${oldMember.displayName}.`);
-			module.exports.handleRolesRemoved(oldMember, newMember, removedRoles);
+			handleRolesRemoved(newMember, removedRoles);
 		}
 
 		const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
 		if (addedRoles.size > 0) {
 			console.debug(`The roles ${addedRoles.map(r => r.name)} were added to ${oldMember.displayName}.`);
-			module.exports.handleRolesAdded(oldMember, newMember, addedRoles);
+			handleRolesAdded(newMember, addedRoles);
 		}
-	},
+	}
+}
 
-	/**
-	 * Handler for when roles are added to a member.
-	 * 
-	 * @param guildMember member that roles were added to
-	 * @param roles roles that were added to member
-	 */
-	handleRolesAdded(guildMember: GuildMember, roles: Collection<Snowflake, Role>) {
-		roles.each(role => {
-			switch (role.name) {
-				case constants.DISCORD_ROLE_GUEST_PASS:
-					try {
-						addGuestPass(guildMember);
-					} catch (e) {
-						console.error(e);
-					}
-					break;
-				case constants.DISCORD_ROLE_DEVELOPERS_GUILD:
-					try {
-						sendGuildWelcomeMessage(guildMember);
-					} catch(e) {
-						console.error(e);
-					}
-					break;
-			}
-		});
-	},
+/**
+ * Handler for when roles are added to a member.
+ * 
+ * @param guildMember member that roles were added to
+ * @param roles roles that were added to member
+ */
+export const handleRolesAdded = (guildMember: GuildMember, roles: Collection<Snowflake, Role>) => { 
+	roles.each(role => {
+		switch (role.name) {
+			case constants.DISCORD_ROLE_GUEST_PASS:
+				try {
+					addGuestPass(guildMember);
+				} catch (e) {
+					console.error(e);
+				}
+				break;
+			case constants.DISCORD_ROLE_DEVELOPERS_GUILD:
+				try {
+					sendGuildWelcomeMessage(guildMember);
+				} catch(e) {
+					console.error(e);
+				}
+				break;
+		}
+	});
+};
 
-	/**
-	 * Handler for when roles are removed from a member.
-	 * 
-	 * @param guildMember member that roles were removed from
-	 * @param roles roles that were removed from member
-	 */
-	handleRolesRemoved(guildMember: GuildMember, roles: Collection<Snowflake, Role>) {
-		roles.each(role => {
-			switch (role.name) {
-				case constants.DISCORD_ROLE_GUEST_PASS:
-					return removeGuestPass(guildMember);
-			}
-		});
-	},
+/**
+ * Handler for when roles are removed from a member.
+ * 
+ * @param guildMember member that roles were removed from
+ * @param roles roles that were removed from member
+ */
+export const handleRolesRemoved = (guildMember: GuildMember, roles: Collection<Snowflake, Role>) => { 
+	roles.each(role => {
+		switch (role.name) {
+			case constants.DISCORD_ROLE_GUEST_PASS:
+				removeGuestPass(guildMember);
+		}
+	});
 };

8e6c70c4b8201992f13d69a16b9326f80d6cab15
Brian Patino <patinobrian@gmail.com>
2021-08-18 20:30:17 -0400

    add error catch

--- src/test/commands/notion/NotionFAQs.test.ts
+++ src/test/commands/notion/NotionFAQs.test.ts
@@ -7,7 +7,7 @@ import * as mockFAQsJsonResponse from './retrieve_faqs_mock.json';
 describe('NotionFAQs', () => {
 	beforeEach(() => {
 		// const env = process.env;
-		process.env = { FAQS_PAGE_ID: '6a2ba0a4-fd1e-4381-b365-6ad5afd418fa' };
+		process.env = { FAQS_PAGE_ID: 'f99e764c-9eb7-4d87-bfc7-fcd93fa93df5' };
 		nock('https://api.notion.com/v1')
 			.get(`/blocks/${process.env.FAQS_PAGE_ID}/children`)
 			.reply(200, mockFAQsJsonResponse)
@@ -16,16 +16,16 @@ describe('NotionFAQs', () => {
 
 	describe('Utilities Validation', () => {
 		it('should be a list of 10 faqs', async () => {
-			const faqs = await retrieveFAQsPromise();
+			const faqs = await retrieveFAQsPromise().catch(console.error);
 			assert.strictEqual(faqs.length, 10);
 		});
 
 		it('first question should be \'1. What is Bankless DAO?\'', async () => {
-			const faqs = await retrieveFAQsPromise();
+			const faqs = await retrieveFAQsPromise().catch(console.error);
 			assert.strictEqual(faqs[0].question, '1. What is Bankless DAO?');
 		});
 		it('first answer should be correct', async () => {
-			const faqs = await retrieveFAQsPromise();
+			const faqs = await retrieveFAQsPromise().catch(console.error);
 			assert.strictEqual(
 				faqs[0].answer,
 				' Bankless DAO is a decentralized community focused on driving adoption and awareness of bankless money systems like Ethereum, Bitcoin and DeFi. You can learn more here:  https://bankless-dao.gitbook.io/bankless-dao/starting-with-bankless-dao ',

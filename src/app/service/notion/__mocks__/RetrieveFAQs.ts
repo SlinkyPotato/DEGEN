f88989ff26105fda08cd40c40764fbe71ec81bc6
Austin Mitchem <austinmitchem@outlook.com>
2021-08-23 21:37:49 -0500

    fix up NotionFAQs.test.ts

---
+++ src/app/service/notion/__mocks__/RetrieveFAQs.ts
@@ -0,0 +1,101 @@
+export default (): any => {
+    return [
+        {
+          question: '1. What is Bankless DAO?',
+          answer: ' Bankless DAO is a decentralized community focused on driving adoption and awareness of bankless money systems like Ethereum, Bitcoin and DeFi. You can learn more here:  https://bankless-dao.gitbook.io/bankless-dao/starting-with-bankless-dao '
+        },
+        {
+          question: '2. How do I join?',
+          answer: ' To join the community and become a member please go to  #start-here . You’ll need 35,000 BANK to access the first level and start contributing. Here’s how you can contribute:  https://bankless-dao.gitbook.io/bankless-dao/starting-with-bankless-dao '
+        },
+        {
+          question: '3. How do I get BANK?',
+          answer: ' You can claim your BANK by visiting the Bankless DAO website  https://www.bankless.community/claim  and connecting your wallet. To qualify for a BANK claim you need to have:\n' +
+            ' - Claimed your Bankless badge before May 4th, 2021\n' +
+            ' - Donated to Bankless through Gitcoin grants\n' +
+            ' - Owned a BAP0 before May 1st, 2021 ‌ For further details of everyone eligible for BANK please digest  https://medium.com/bankless-dao/announcing-bankless-dao-133220f5efd8 ‌'
+        },
+        {
+          question: '4. I didn’t qualify for BANK distribution. How do I get BANK?',
+          answer: ' The Bankless DAO is already discussing a further distribution of BANK to premium subscribers and the lowering of the 35,000 BANK threshold to Discord membership. You can also swap ERC-20 tokens or ether for BANK on popular decentralized exchanges:\n' +
+            ' - Uniswap V2 ETH/BANK Pair\n' +
+            ' - Uniswap V3 ETH/BANK Pair'
+        },
+        {
+          question: '5. What’s the purpose of BANK?',
+          answer: ' The goal of BANK is to create a tokenized vehicle to coordinate actions in line with the DAO’s mission. You can learn more about the Mission, Vision and Core Values of the Bankless DAO here:  https://bankless-dao.gitbook.io/bankless-dao/starting-with-bankless-dao  ‌'
+        },
+        {
+          question: '6. How can I contribute?',
+          answer: ' Share your skills and step up to help the Bankless DAO achieve its mission. When you become a member of the Discord, go to #intros and tell us about yourself and how you can contribute. Here’s the getting started with Bankless DAO guide  https://medium.com/bankless-dao/getting-started-with-bankless-dao-94b0b60e052e  ‌'
+        },
+        {
+          question: '7. How do I vote on proposals?',
+          answer: ' Help guide the future of Bankless DAO by voting on proposals with the BANK in your wallet. Here’s the official Snapshot page  https://snapshot.org/#/banklessvault.eth  where proposals are open for three days of voting. ‌'
+        },
+        {
+          question: '8. What is the Genesis Proposal?',
+          answer: ' The Genesis Proposal has closed for voting but is essential reading for DAO contributors, as it lays the foundation for key infrastructure of the Bankless DAO  https://snapshot.org/#/banklessvault.eth/proposal/QmdoixPMMT76vSt6ewkE87JZJywS1piYsGC3nJJpcrPXKS      The Genesis Proposal is the first proposal by Bankless LLC to grant a portion of the community treasury to the company in order to act as core participants in the DAO. Since BANK was a fair launch, Bankless LLC did not receive any BANK. It’s up to the community to choose its path. ‌'
+        },
+        {
+          question: '9. I donated to Bankless through Gitcoin but didn’t get BANK?',
+          answer: ' If you donated more than $0.95 to Bankless through Gitcoin, but your address shows ‘0 BANK’ when you try and claim https://www.bankless.community/claim, please send a DM to our community manager @Above Average Joe with the details of your donation. ‌'
+        },
+        {
+          question: '10. I tried joining but didn’t receive a response from the Collab.Land bot?',
+          answer: ' Check that your Discord settings allow you to receive direct messages and make sure that the wallet you are connecting with has 35,000 BANK. Type !join in #join and try again. ‌'
+        },
+        {
+          question: '11. I’m still having problems trying to join Discord?',
+          answer: ' Try using the latest version of MetaMask  https://metamask.io/download.html  to join Discord. You can import your wallet or send BANK to a new address to join. If you are still struggling please ask for help in the #support channel. ‌'
+        },
+        {
+          question: '12. Do I need to hold $BANK to maintain my status in the Discord?',
+          answer: ' Yes, you will need to hold $BANK in your wallet that is connected to collab.land bot in order to maintain your status in Discord and not be removed by the bot. ‌'
+        },
+        {
+          question: '13. Can collab.land bot “trace” the address associated with my Discord to a yield farm or LP and allow me to maintain my status?',
+          answer: ' No the bot can’t track your status to a yield farm or LP token, if you move your $BANK tokens from your associated wallet, you will be unable to maintain your status in the Bankless DAO Discord. ‌'
+        },
+        {
+          question: '14. What do the different level channels represent?\n',
+          answer: '\n' +
+            ' - 🧑‍Member (Level 1) : 35,000 BANK\n' +
+            ' - 🤝 Contributor  ( Level 2 ): 35,000 BANK +  Nomination/Vote process .\n' +
+            ' - 🐋 Whale (Level 3) : 150,000 BANK\n' +
+            ' - 🌊 Liquidity Provider  (Level 4): 250 Uniswap V2 BANK/ETH LP tokens As you move up levels, you get access to additional channels that will enable you to contribute in certain areas to the DAO, for example business development, social media management, design, etc ‌'
+        },
+        {
+          question: '15. Who do I contact about a listing proposal for BANK?',
+          answer: ' BANK is an ERC-20 token and can be listed freely by any exchange. ‌'
+        },
+        {
+          question: '16. What are Guilds? How do I join?',
+          answer: ' Untitled  '
+        },
+        {
+          question: '17. How do I edit Notion?',
+          answer: ' Ask for Guest access from aboveaveragejoe, frogmonkee, kouros, 0xLucas, or wolfehr on Discord'
+        },
+        {
+          question: '18: How can I earn Bank?',
+          answer: '\n' +
+            ' - Tips from other bankless members for valuable or desirable contributions in the moment. (This can be for work done, well spoken comments, answering other member questions, or whatever else the tipper values)\n' +
+            ' - Bounties posted in the  #🧀-bounty-board  channel. Bounties are claimed upon delivery of requested action or material.\n' +
+            ' - Grants. Bankless grants are planned for outstanding effort to assist in the growth of the Dao, or for other items as determined by the grants committee. (Coming soon)\n' +
+            ' - Contributor rewards. Retroactive remuneration for part to full time contribution to the development of the Dao, to be decided by each guild respectively. (Coming soon)'
+        },
+        {
+          question: "19: I don't have enough Bank to be a full member, and can only speak in the level 0 area. How can I get more involved?",
+          answer: ' Find an area of the Dao where you feel you may be able to contribute, and reach out to anyone with the  @Ops Guild  tag or  @Contributors (Lvl 2)  tag. Let them know where you would like to be involved, or what you have been working on to add value to the dao. You can also speak in  #🎢get-involved  to find your connection point. Those who contribute are given guest passes into the Dao, and are allowed to retain them until either they go inactive or achieve level 1 status through earnings for contributions.'
+        },
+        {
+          question: '20. When are community calls?',
+          answer: ' Community calls are every Friday at 11am EDT ---> https://www.worldtimebuddy.com/  to convert locally'
+        },
+        {
+          question: '21. I have an idea, how do I make a proposal?',
+          answer: ' Read under  How to Gather Consensus  on this page:  Untitled  '
+        }
+      ]
+};

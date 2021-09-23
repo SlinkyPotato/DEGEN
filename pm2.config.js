f53bd3a599b80806d51cfc136e997a0b21d6c0ef
Brian <patinobrian@gmail.com>
2021-09-23 14:47:27 -0400

    add timestamp to config

--- pm2.config.js
+++ pm2.config.js
@@ -3,5 +3,6 @@ module.exports = {
 		name   : 'DEGEN',
 		script : './dist/app/app.js',
 		node_args: '-r dotenv/config --trace-warnings dist/app/app.js dotenv_config_path=.env.qa',
+		log_date_format: 'YYYY-MM-DD HH:mm Z',
 	}],
 };

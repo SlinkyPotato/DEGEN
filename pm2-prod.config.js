97e09979793b16e9d1264a76f463516194089673
Brian Patino <Patinobrian@gmail.com>
2021-10-10 00:00:27 -0400

    use single config and specify app

--- pm2-prod.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	apps : [{
-		name   : 'DEGEN',
-		script : './dist/app/app.js',
-		node_args: '-r dotenv/config --trace-warnings dist/app/app.js dotenv_config_path=.env.prod',
-		log_date_format: 'YYYY-MM-DD HH:mm Z',
-	}],
-};

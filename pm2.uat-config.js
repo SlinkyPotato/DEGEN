01498fd2b41f5d995e33d1386808fda480d28f85
Brian Patino <Patinobrian@gmail.com>
2021-10-09 23:37:51 -0400

    add uat config

---
+++ pm2.uat-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	apps : [{
+		name   : 'uat-bot',
+		script : './dist/app/app.js',
+		node_args: '-r dotenv/config --trace-warnings dist/app/app.js dotenv_config_path=.env.qa',
+		log_date_format: 'YYYY-MM-DD HH:mm Z',
+	}],
+};

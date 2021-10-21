42237708c327df4a5f0c337604ea477696b9ebec
Brian <patinobrian@gmail.com>
2021-10-21 19:30:30 -0400

    wip

---
+++ src/app/commands/admin/Verify.ts
@@ -0,0 +1,44 @@
+import { CommandContext, CommandOptionType, SlashCommand, SlashCreator } from 'slash-create';
+import Log, { LogUtils } from '../../utils/Log';
+
+export default class Verify extends SlashCommand {
+	constructor(creator: SlashCreator) {
+		super(creator, {
+			name: 'verify',
+			description: 'Verify ownership of external accounts and wallets',
+			throttling: {
+				usages: 1,
+				duration: 2,
+			},
+			defaultPermission: true,
+			options: [
+				{
+					name: 'account',
+					type: CommandOptionType.SUB_COMMAND,
+					description: 'Link DEGEN to your account or wallet.',
+					options: [
+						{
+							name: 'type-verify',
+							type: CommandOptionType.STRING,
+							description: 'Type of account or wallet to verify and link.',
+							required: true,
+							choices: [
+								{
+									name: 'Twitter Spaces',
+									value: 'TWITTER_SPACES',
+								},
+							],
+						},
+					],
+				},
+			],
+		});
+	}
+	
+	async run(ctx: CommandContext): Promise<any> {
+		LogUtils.logCommandStart(ctx);
+		if (ctx.user.bot) return;
+		
+		Log.info(ctx.user.avatarURL);
+	}
+}
--- src/app/service/constants/discordServerIds.ts
+++ src/app/service/constants/discordServerIds.ts
@@ -1,5 +1,5 @@
 export default Object.freeze({
-	banklessDAO: '834499078434979890',
+	banklessDAO: '850840267082563596',
 	discordBotGarage: '851552281249972254',
 	slinkyPotatoServer: '850840267082563596',
 });

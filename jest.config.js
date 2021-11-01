b018b3e5bf3b841efca53a9c2ffe9bd5add99ee1
jonvaljonathan.eth <jonathan42white@gmail.com>
2021-11-01 18:58:23 -0500

    imporved test suite

--- jest.config.js
+++ jest.config.js
@@ -28,12 +28,10 @@ process.env.DISCORD_BOT_PUBLIC_KEY = 'sdafsdafasdfsdaf';
 process.env.DISCORD_BOT_TOKEN =
     'asfsadf.YLsadfvgyg.106GODufx1masdfsdaf8JwsPdru0KWuG8BY';
 process.env.DISCORD_OWNER_ID = '23432434';
-process.env.MONGODB_USERNAME="dbadmin2";
-process.env.MONGODB_PASS="RTnWmSldFReOdhUS";
-process.env.MONGODB_CLUSTER="cluster0-7znrv.mongodb.net/test";
-process.env.MONGODB_PREFIX="mongodb+srv";
-
-process.env.NOTION_TOKEN = 'cluster0-7znrv.mongodb.net/test';
+process.env.MONGODB_USERNAME = 'user';
+process.env.MONGODB_PASS = 'asfsdfasf';
+process.env.MONGODB_CLUSTER = 'clusasfdasdf.asdfsd';
+process.env.NOTION_TOKEN = 'secret+asfsdfsdfasdfsadf';
 process.env.FAQS_PAGE_ID = '6a2ba0a4-fd1e-4381-b365-6ad5afd418fa';
 process.env.DISCORD_SERVER_ID = '24332423423';
 process.env.DISCORD_ROLE_GUEST_PASS = '857683488131907624';

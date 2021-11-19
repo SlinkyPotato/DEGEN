a1f0ebf321a73a1272ba7c0309ac4e66ded75182
Behold <Behold.3th@gmail.com>
2021-11-18 21:42:28 -0500

    feature/bountyboard_multitenancy: expanded regex for Hi bounty board event

--- src/app/events/chat/MessageCreateOnBB.ts
+++ src/app/events/chat/MessageCreateOnBB.ts
@@ -2,7 +2,7 @@ import { Message } from 'discord.js';
 
 const MessageCreateOnBB = async (message: Message): Promise<any> => {
 	const greetings = ['Hello', 'Howdy', 'Hey', 'Go Bankless,', 'Nice to meet you,', 'It\'s great to see you,', 'Ahoy,'];
-	if (message.content.toLowerCase().match('^.*bounty board$')) {
+	if (message.content.toLowerCase().match('^.*bounty board[!.?]$')) {
 		message.channel.send({ content: `${greetings[Math.floor(Math.random() * greetings.length)]} ${message.author.username}!` });
 	}
 	return;

fe8cc560ff2d07fb98617de2a563df682af25777
olufemi-olumaiyegun <femimayegun@gmail.com>
2022-03-18 17:15:50 -0500

    remove additional mongodb container

--- src/app/events/tracking/HandleParticipantDuringEvent.ts
+++ src/app/events/tracking/HandleParticipantDuringEvent.ts
@@ -25,38 +25,17 @@ const HandleParticipantDuringEvent = async (oldState: VoiceState, newState: Voic
 	if (hasUserBeenDeafened(oldState, newState)) {
 		if (await isChannelActivePOAPEvent(oldState.channelId, oldState.guild.id)) {
 			Log.log(`user has deafened for previous channel, userId: ${newState.id}`);
-			await removeDeafenedUser(oldState.channelId, oldState.guild.id, oldState.id, oldState.member?.user.tag);
+			await removeDeafenedUser(oldState.channelId, oldState.guild.id, oldState.id);
 		}
 		if (newState.channelId != oldState.channelId && await isChannelActivePOAPEvent(newState.channelId, newState.guild.id)) {
 			Log.log(`user has deafened for new channel, userId: ${newState.id}`);
-			await removeDeafenedUser(newState.channelId, newState.guild.id, newState.id, oldState.member?.user.tag);
+			await removeDeafenedUser(newState.channelId, newState.guild.id, newState.id);
 		}
 		return;
 	}
 	
 	if (hasUserBeenUnDeafened(oldState, newState)) {
 		if (await isChannelActivePOAPEvent(newState.channelId, newState.guild.id)) {
-			const db: Db = await MongoDbUtils.connect(constants.DB_NAME_DEGEN);
-			const addtionalPoapParticipantsCol: Collection<POAPParticipant> = db.collection(constants.DB_COLLECTION_ADDITIONAL_POAP_PARTICIPANTS_LIST);
-			const undeafenedUser = retrieveParticipantFromAdditionalList({ id: newState.id, tag: newState.member?.user.tag }, newState.channelId, newState.guild.id);
-			const undeafenedUserQuery: POAPParticipant | null = await addtionalPoapParticipantsCol.findOne({
-				discordUserId: newState.id,
-			});
-			let currentMinutesDeafened = 0.0;
-			if(undeafenedUserQuery?.minutesDeafened) {
-				currentMinutesDeafened = undeafenedUserQuery?.minutesDeafened;
-			}
-			const timediff: number = dayjs().diff(undeafenedUserQuery?.timeDeafened, 'm', true);
-			const minutesDeafened: number = timediff + currentMinutesDeafened;
-			const resultUpdate: UpdateWriteOpResult | void = await addtionalPoapParticipantsCol.updateOne(undeafenedUser, {
-				$set: {
-					minutesDeafened: minutesDeafened,
-					timeDeafened: '',
-				},
-			}).catch(Log.error);
-			if (!resultUpdate) {
-				Log.error('Failed to update addtional participant info in db');
-			}
 			Log.log(`user has undeafened for new channel, userId: ${newState.id}`);
 			await startTrackingUserParticipation({ id: newState.id, tag: newState.member?.user.tag }, newState.guild.id, newState.channelId);
 		}
@@ -68,43 +47,7 @@ const HandleParticipantDuringEvent = async (oldState: VoiceState, newState: Voic
 	
 	if (hasUserChangedChannels(oldState, newState)) {
 		if (await isChannelActivePOAPEvent(oldState.channelId, oldState.guild.id)) {
-			const db: Db = await MongoDbUtils.connect(constants.DB_NAME_DEGEN);
-			const addtionalPoapParticipantsCol: Collection<POAPParticipant> = db.collection(constants.DB_COLLECTION_ADDITIONAL_POAP_PARTICIPANTS_LIST);
-			const leavingUser = retrieveParticipantFromAdditionalList({ id: oldState.id, tag: oldState.member?.user.tag }, oldState.channelId, oldState.guild.id);
-			const leavingUserQuery: POAPParticipant | null = await addtionalPoapParticipantsCol.findOne({
-				discordUserId: oldState.id,
-			});
-			const participant = await retrieveActiveParticipant({ id: oldState.id, tag: oldState.member?.user.tag }, oldState.channelId, `${oldState.member?.user.tag}`);
-			if (!leavingUserQuery) {
-				const minutesAttended = dayjs().diff(participant?.startTime, 'm', true);
-				const resultInsert: InsertOneWriteOpResult<POAPParticipant> | void = await addtionalPoapParticipantsCol.insertOne({
-					discordUserId: oldState.id,
-					discordUserTag: oldState.member?.user.tag,
-					voiceChannelId: oldState.channelId,
-					minutesAttended:  minutesAttended,
-					timeLeft: dayjs().toISOString(),
-				} as POAPParticipant).catch(Log.error);
-				if (resultInsert) {
-					Log.error('Failed to insert participant additional info in db');
-				}
-			} else {
-				let initialDuration = 0.0;
-				if(leavingUserQuery?.minutesAttended) {
-					initialDuration = leavingUserQuery?.minutesAttended;
-				}
-				const minutesAttended = initialDuration + dayjs().diff(participant?.startTime, 'm', true);
-				const resultInsert: UpdateWriteOpResult | void = await addtionalPoapParticipantsCol.updateOne(leavingUser, {
-					$set: {
-						minutesAttended: minutesAttended,
-						timeLeft: dayjs().toISOString(),
-					},
-				}).catch(Log.error);
-				if (!resultInsert) {
-					Log.error('Failed to update participant additional info in db');
-				}
-			}
 			await stopTrackingUserParticipation({ id: oldState.id, tag: oldState.member?.user.tag }, oldState.guild.id, oldState.channelId, null);
-
 		}
 		if (await isChannelActivePOAPEvent(newState.channelId, newState.guild.id)) {
 			await startTrackingUserParticipation({ id: newState.id, tag: newState.member?.user.tag }, newState.guild.id, newState.channelId);
@@ -153,45 +96,14 @@ const isChannelActivePOAPEvent = async (
 	return false;
 };
 
-const removeDeafenedUser = async (channelId: string | null, guildId: string | null, userId: string | null, userTag: string | undefined) => {
+const removeDeafenedUser = async (channelId: string | null, guildId: string | null, userId: string | null) => {
 	if (channelId == null || guildId == null || userId == null) {
 		return;
 	}
 	
 	const db: Db = await MongoDbUtils.connect(constants.DB_NAME_DEGEN);
 	const poapParticipantsCol: Collection<POAPParticipant> = db.collection(constants.DB_COLLECTION_POAP_PARTICIPANTS);
-	const addtionalPoapParticipantsCol: Collection<POAPParticipant> = db.collection(constants.DB_COLLECTION_ADDITIONAL_POAP_PARTICIPANTS_LIST);
-	const undeafenedUser = retrieveParticipantFromAdditionalList({ id: userId, tag: userTag }, channelId, guildId);
-	const participant = await retrieveActiveParticipant({ id: userId, tag: userTag }, channelId, guildId);
-	const undeafenedUserQuery: POAPParticipant | null = await addtionalPoapParticipantsCol.findOne({
-		discordUserId: userId,
-	});
-	let minutesListened = 0.0;
-	if (undeafenedUserQuery?.minutesListened) {
-		minutesListened = undeafenedUserQuery?.minutesListened;
-	}
-	if (undeafenedUser) {
-		await addtionalPoapParticipantsCol.updateOne(undeafenedUser, {
-			$set: {
-				timeDeafened: dayjs().toISOString(),
-				minutesListened: minutesListened + dayjs().diff(participant?.startTime, 'm', true),
-			},
-		}).catch(Log.error);
-	}else {
-		const resultInsert: InsertOneWriteOpResult<POAPParticipant> | void = await addtionalPoapParticipantsCol.insertOne({
-			discordUserId: userId,
-			voiceChannelId: channelId,
-			timeDeafened: dayjs().toISOString(),
-			discordServerId: guildId,
-			minutesDeafened: 0,
-			discordUserTag: userTag,
-			minutesListened: dayjs().diff(participant?.startTime, 'm', true),
-		} as POAPParticipant).catch(Log.warn);
-		if (resultInsert == null || resultInsert.insertedCount !== 1) {
-			throw new MongoError('failed to insert poapParticipant');
-		}
-	}
-
+	
 	const result: DeleteWriteOpResultObject | void = await poapParticipantsCol.deleteOne({
 		voiceChannelId: channelId,
 		discordServerId: guildId,
@@ -253,8 +165,10 @@ export const stopTrackingUserParticipation = async (user: BasicUser, guildId: st
 	if (participant == null) {
 		throw new MongoError('could not find participant in db when trying to stop tracking');
 	}
+	
 	channelId = channelId as string;
 	guildId = guildId as string;
+	
 	const durationInMinutes: number = calculateDuration(participant.startTime, participant.durationInMinutes);
 	const db: Db = await MongoDbUtils.connect(constants.DB_NAME_DEGEN);
 	const poapParticipantsDb: Collection<POAPParticipant> = db.collection(constants.DB_COLLECTION_POAP_PARTICIPANTS);
@@ -264,39 +178,7 @@ export const stopTrackingUserParticipation = async (user: BasicUser, guildId: st
 			durationInMinutes: durationInMinutes,
 		},
 	}).catch(Log.error);
-	const addtionalPoapParticipantsCol: Collection<POAPParticipant> = db.collection(constants.DB_COLLECTION_ADDITIONAL_POAP_PARTICIPANTS_LIST);
-	const leavingUser = retrieveParticipantFromAdditionalList(user, channelId, guildId);
-	const leavingUserQuery: POAPParticipant | null = await addtionalPoapParticipantsCol.findOne({
-		discordUserId: user.id,
-	});
-	if (!leavingUserQuery) {
-		const minutesAttended = dayjs().diff(participant.startTime, 'm', true);
-		const resultInsert: InsertOneWriteOpResult<POAPParticipant> | void = await addtionalPoapParticipantsCol.insertOne({
-			discordUserId: user.id,
-			discordUserTag: user.tag,
-			voiceChannelId: channelId,
-			minutesAttended:  minutesAttended,
-			timeLeft: dayjs().toISOString(),
-		} as POAPParticipant).catch(Log.error);
-		if (!resultInsert) {
-			Log.debug('Failed to insert participant additional info in db');
-		}
-	} else {
-		let initialDuration = 0.0;
-		if(leavingUserQuery?.minutesAttended) {
-			initialDuration = leavingUserQuery?.minutesAttended;
-		}
-		const minutesAttended = initialDuration + dayjs().diff(participant.startTime, 'm', true);
-		const resultInsert: UpdateWriteOpResult | void = await addtionalPoapParticipantsCol.updateOne(leavingUser, {
-			$set: {
-				minutesAttended: minutesAttended,
-				timeLeft: dayjs().toISOString(),
-			},
-		}).catch(Log.error);
-		if (!resultInsert) {
-			Log.error('Failed to update participant additional info in db');
-		}
-	}
+	
 	if (result == null || result.result.ok != 1) {
 		throw new MongoError('failed to update present participant in db');
 	}
@@ -327,18 +209,5 @@ const retrieveActiveParticipant = async (
 		discordServerId: guildId,
 	});
 };
-const retrieveParticipantFromAdditionalList = async (
-	user: BasicUser, channelId: string | null, guildId: string,
-): Promise<POAPParticipant | null> => {
-	if (user.id == null || channelId == null || guildId == null) {
-		return null;
-	}
-	const db: Db = await MongoDbUtils.connect(constants.DB_NAME_DEGEN);
-	const addPoapParticipantsDb: Collection<POAPParticipant> = db.collection(constants.DB_COLLECTION_ADDITIONAL_POAP_PARTICIPANTS_LIST);
-	return await addPoapParticipantsDb.findOne({
-		discordUserId: user.id,
-		voiceChannelId: channelId,
-		discordServerId: guildId,
-	});
-};
+
 export default HandleParticipantDuringEvent;
